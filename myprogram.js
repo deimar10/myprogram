21651240281103586723813a6299c544df69f766
deimar <deimar.parn@khk.ee>
2022-01-24 17:32:53 +0000

    The EventEmitter Object
    To fire an event, use the emit() method.

--- myprogram.js
+++ myprogram.js
@@ -2,9 +2,9 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
     fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-    res.end('Tere maailm!');
+        res.end('Tere maailm!');
     });
 }).listen(8080);
 
@@ -15,5 +15,18 @@ var fs = require('fs');
 //create a file named mynewfile1.txt:
 fs.writeFile('mynewfile1.txt', 'This is my new content', function (err) {
     if (err) throw err;
-    console.log('Replace');
 });
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
